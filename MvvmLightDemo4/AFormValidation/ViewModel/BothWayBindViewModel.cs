4ffd2d963d7b5c09586e08bae295215a40c18cd7
york <2737365793@qq.com>
2018-11-08 10:30:20 +0800

    xxx

---
+++ MvvmLightDemo4/AFormValidation/ViewModel/BothWayBindViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using MvvmLightDemo4.Model;
+using System.Threading.Tasks;
+using MvvmLightDemo4.AModelBinding.Model;
+namespace MvvmLightDemo4.AFormValidation.ViewModel
+{
+    
+    public class BothWayBindViewModel : ViewModelBase
+    {
+        #region 属性
+        private UserInfoModel userInfo;
+        public UserInfoModel UserInfo
+        {
+            get { return userInfo; }
+            set { userInfo = value;RaisePropertyChanged(()=>UserInfo); }
+
+        }
+        #endregion
+        public BothWayBindViewModel()
+
+        {
+            UserInfo = new UserInfoModel();
+            UserInfo.UserName = "york";
+        }
+
+    }
+}
